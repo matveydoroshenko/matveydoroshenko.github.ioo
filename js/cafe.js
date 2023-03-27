dfea8a9d3cc01c6797a1a693c97ca158e7293424
matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>
2023-03-27 16:35:23 +0300

    Update cafe.js
    Signed-off-by: matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>

--- js/cafe.js
+++ js/cafe.js
@@ -249,6 +249,7 @@
       Cafe.updateTotalPrice();
     },
     mainBtnClicked: function() {
+      WebApp.showAlert("Хорошо, ты нажал на главную кнопку.");
       if (!Cafe.canPay || Cafe.isLoading || Cafe.isClosed) {
         return false;
       }
