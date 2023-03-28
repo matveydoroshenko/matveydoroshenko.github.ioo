46e84e98ae5bfee14d0ed726daf98d4187bd4a96
matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>
2023-03-28 17:45:23 +0300

    Update PLR.js
    Signed-off-by: matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>

--- PLR/PLR.js
+++ PLR/PLR.js
@@ -1,3 +1,36 @@
-var WebApp = window.Telegram.WebApp;
+(function($) {
+    $.fn.redraw = function() {
+      return this.map(function(){ this.offsetTop; return this; });
+    };
+  })(jQuery);
 
-WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
+  var Cafe = {
+    canPay: true,
+    modeOrder: true,
+    totalPrice: 29,
+
+    init: function(options) {
+      Telegram.WebApp.ready();
+      Cafe.apiUrl = options.apiUrl;
+      Cafe.userId = options.userId;
+      Cafe.userHash = options.userHash;
+      Cafe.initLotties();
+      var userId = Telegram.WebApp.initData && Telegram.WebApp.initData.user && Telegram.WebApp.initData.user.id || Cafe.userId;
+      if(options.debug) {
+        var userId = 2349000000;
+      }
+      $('body').show();
+      if (!userId) {
+        Cafe.isClosed = true;
+        $('body').addClass('closed');
+        Cafe.showStatus('Cafe is temporarily closed');
+        return;
+      }
+      $('.js-item-lottie').on('click', Cafe.eLottieClicked);
+      $('.js-item-incr-btn').on('click', Cafe.eIncrClicked);
+      $('.js-item-decr-btn').on('click', Cafe.eDecrClicked);
+      $('.js-order-edit').on('click', Cafe.eEditClicked);
+      $('.js-status').on('click', Cafe.eStatusClicked);
+      $('.js-order-comment-field').each(function() {
+        autosize(this);
+      });
