bd47276d7e463a06e123782d198a68873d55effd
matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>
2023-04-01 16:46:56 +0300

    Update scripts.js
    Signed-off-by: matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>

--- scripts.js
+++ scripts.js
@@ -1,47 +1,94 @@
-const productList = document.getElementById('productList');
-const cartList = document.getElementById('cartList');
-const checkoutBtn = document.getElementById('checkoutBtn');
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const products = [
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 2, name: 'Product 2', price: 200 },
-    { id: 3, name: 'Product 3', price: 300 },
-]
+// Initial product data
+const initialProducts = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 10,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 20,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 30,
+    image: "https://via.placeholder.com/150",
+  },
+];
 
-const cart = [];
+// App component
+const App = () => {
 
-function addProductToCart(product) {
-    const existingProduct = cart.find(item => item.id === product.id);
-    if (existingProduct) {
-        existingProduct.quantity++;
-        document.getElementById(productQuantity-${product.id}).textContent = existingProduct.quantity;
+  // Create state variables
+  const [products, setProducts] = useState(initialProducts);
+  const [cartItems, setCartItems] = useState([]);
+
+  // Add to cart function
+  const addToCart = (product) => {
+    // Check if item already exists in cart
+    const foundItem = cartItems.find(item => item.id === product.id);
+    if (foundItem) {
+      // Increase quantity of existing item
+      const updatedCart = cartItems.map(item => item.id === foundItem.id ? { ...item, quantity: item.quantity + 1 } : item);
+      setCartItems(updatedCart);
     } else {
-        const newCartItem = { ...product, quantity: 1 };
-        cart.push(newCartItem);
-
-        const cartItem = document.createElement('div');
-        cartItem.className = 'cart-item';
-        cartItem.innerHTML =
-            <h2>${product.name}</h2>
-            <p>Price: $${product.price}</p>
-            <p id="productQuantity-${product.id}">Quantity: 1</p>
-        ;
-        cartList.appendChild(cartItem);
+      // Add new item to cart
+      const newItem = { ...product, quantity: 1 };
+      setCartItems([...cartItems, newItem]);
     }
-}
+  };
 
-products.forEach(product => {
-    const productItem = document.createElement('div');
-    productItem.className = 'product';
-    productItem.innerHTML =
-        <h2>${product.name}</h2>
-        <p>Price: $${product.price}</p>
-        <button onclick="addProductToCart(${JSON.stringify(product).split('"').join(""")})">Add to Cart</button>
-    ;
-    productList.appendChild(productItem);
-});
+  // Update cart item function
+  const updateCartItem = (id, quantity) => {
+    const updatedCart = cartItems.map(item => item.id === id ? { ...item, quantity: quantity } : item);
+    setCartItems(updatedCart);
+  };
 
-checkoutBtn.addEventListener('click', () => {
-    alert('Thank you for your purchase!');
-    window.location.reload();
-});
+  // Remove cart item function
+  const removeCartItem = (id) => {
+    const updatedCart = cartItems.filter(item => item.id !== id);
+    setCartItems(updatedCart);
+  };
+
+  // Calculate total price of items in cart
+  const total = cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <div className="product-list">
+        {products.map(product => (
+          <div className="product" key={product.id}>
+            <img src={product.image} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>Price: ${product.price}</p>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </div>
+        ))}
+      </div>
+      <h1>Shopping Cart</h1>
+      <div className="cart-items">
+        {cartItems.map(item => (
+          <div className="cart-item" key={item.id}>
+            <img src={item.image} alt={item.name} />
+            <h2>{item.name}</h2>
+            <p>Price: ${item.price}</p>
+            <input type="number" min="1" value={item.quantity} onChange={(e) => updateCartItem(item.id, parseInt(e.target.value))} />
+            <button onClick={() => removeCartItem(item.id)}>Remove</button>
+          </div>
+        ))}
+      </div>
+      <h2>Total: ${total}</h2>
+    </div>
+  );
+};
+
+// Render the app
+ReactDOM.render(<App />, document.getElementById("root"));
