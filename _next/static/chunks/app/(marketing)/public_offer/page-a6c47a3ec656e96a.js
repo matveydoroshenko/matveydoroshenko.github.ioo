1452fc5bf867ce1b42c31896b8763b096e49836c
matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>
2023-04-16 16:44:16 +0300

    Add files via upload
    Signed-off-by: matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>

---
+++ _next/static/chunks/app/(marketing)/public_offer/page-a6c47a3ec656e96a.js
@@ -0,0 +1,11 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[484, 931], {
+    2716: function(n, e, u) {
+        Promise.resolve().then(u.t.bind(u, 2104, 23))
+    }
+}, function(n) {
+    n.O(0, [104, 679, 744], function() {
+        return n(n.s = 2716)
+    }),
+    _N_E = n.O()
+}
+]);
